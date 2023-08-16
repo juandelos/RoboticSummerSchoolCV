2eed9e0ce28c387ad10b3f4d7af7ea011cd7a38c
Juan <juan@MacBook-Pro-de-Juan.local>
2023-08-16 10:59:31 +0200

    initial commit

---
+++ resnet.py
@@ -0,0 +1,203 @@
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from torchvision.models import ResNet18_Weights
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+
+# DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+DEVICE = torch.device("cpu")
+class ResNet():
+    def main(self):
+        cudnn.benchmark = True
+        plt.ion()   # interactive mode
+
+        # Data augmentation and normalization for training
+        # Just normalization for validation
+        self.data_transforms = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+
+        data_dir = 'vision_project'
+        self.image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                                self.data_transforms[x])
+                        for x in ['train', 'val']}
+        self.dataloaders = {x: torch.utils.data.DataLoader(self.image_datasets[x], batch_size=4,
+                                                    shuffle=True, num_workers=4)
+                    for x in ['train', 'val']}
+        self.dataset_sizes = {x: len(self.image_datasets[x]) for x in ['train', 'val']}
+        self.class_names = self.image_datasets['train'].classes
+
+        self.device = torch.device(DEVICE)
+
+
+
+
+        # Get a batch of training data
+        inputs, classes = next(iter(self.dataloaders['train']))
+
+        # Make a grid from batch
+        out = torchvision.utils.make_grid(inputs)
+
+        self.imshow(out, title=[self.class_names[x] for x in classes])
+
+
+
+
+
+
+        model_ft = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+        num_ftrs = model_ft.fc.in_features
+        # Here the size of each output sample is set to 2.
+        # Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
+        model_ft.fc = nn.Linear(num_ftrs, 4)
+
+        model_ft = model_ft.to(self.device)
+
+        criterion = nn.CrossEntropyLoss()
+
+        # Observe that all parameters are being optimized
+        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+        # Decay LR by a factor of 0.1 every 7 epochs
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+        model_ft = self.train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                            num_epochs=25)
+
+        self.visualize_model(model_ft)
+
+    def visualize_model(self, model, num_images=6):
+        was_training = model.training
+        model.eval()
+        images_so_far = 0
+        fig = plt.figure()
+
+        with torch.no_grad():
+            for i, (inputs, labels) in enumerate(self.dataloaders['val']):
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+
+                for j in range(inputs.size()[0]):
+                    images_so_far += 1
+                    ax = plt.subplot(num_images//2, 2, images_so_far)
+                    ax.axis('off')
+                    ax.set_title(f'predicted: {self.class_names[preds[j]]}')
+                    self.imshow(inputs.cpu().data[j])
+
+                    if images_so_far == num_images:
+                        model.train(mode=was_training)
+                        return
+            model.train(mode=was_training)
+
+    def train_model(self, model, criterion, optimizer, scheduler, num_epochs=25):
+        since = time.time()
+
+        # Create a temporary directory to save training checkpoints
+        with TemporaryDirectory() as tempdir:
+            best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+            torch.save(model.state_dict(), best_model_params_path)
+            best_acc = 0.0
+
+            for epoch in range(num_epochs):
+                print(f'Epoch {epoch}/{num_epochs - 1}')
+                print('-' * 10)
+
+                # Each epoch has a training and validation phase
+                for phase in ['train', 'val']:
+                    if phase == 'train':
+                        model.train()  # Set model to training mode
+                    else:
+                        model.eval()   # Set model to evaluate mode
+
+                    running_loss = 0.0
+                    running_corrects = 0
+
+                    # Iterate over data.
+                    for inputs, labels in self.dataloaders[phase]:
+                        inputs = inputs.to(self.device)
+                        labels = labels.to(self.device)
+
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
+
+                        # forward
+                        # track history if only in train
+                        with torch.set_grad_enabled(phase == 'train'):
+                            outputs = model(inputs)
+                            _, preds = torch.max(outputs, 1)
+                            loss = criterion(outputs, labels)
+
+                            # backward + optimize only if in training phase
+                            if phase == 'train':
+                                loss.backward()
+                                optimizer.step()
+
+                        # statistics
+                        running_loss += loss.item() * inputs.size(0)
+                        running_corrects += torch.sum(preds == labels.data)
+                    if phase == 'train':
+                        scheduler.step()
+
+                    epoch_loss = running_loss / self.dataset_sizes[phase]
+                    epoch_acc = running_corrects.double() / self.dataset_sizes[phase]
+
+                    print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                    # deep copy the model
+                    if phase == 'val' and epoch_acc > best_acc:
+                        best_acc = epoch_acc
+                        torch.save(model.state_dict(), best_model_params_path)
+
+                print()
+
+            time_elapsed = time.time() - since
+            print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+            print(f'Best val Acc: {best_acc:4f}')
+
+            # load best model weights
+            model.load_state_dict(torch.load(best_model_params_path))
+        return model
+    
+    def imshow(self, inp, title=None):
+        """Display image for Tensor."""
+        inp = inp.numpy().transpose((1, 2, 0))
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        inp = std * inp + mean
+        inp = np.clip(inp, 0, 1)
+        plt.imshow(inp)
+        if title is not None:
+            plt.title(title)
+        plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+if __name__ == '__main__':
+    obj = ResNet()
+    obj.main()
